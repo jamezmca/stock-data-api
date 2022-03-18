7cb2eca484d6734e6a70ab4505fa387d0a70715c
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-17 21:40:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/:ticker', async (req, res) => {
     }
     const { data } = await axios.get('https://finance.yahoo.com/quote/MRNA/key-statistics?p=MRNA')
     const $ = cheerio.load(data)
-    return res.send({ data: $('section[data-test="qsp-statistics"] > div:nth-child(2) tr').get().map(val => $(val).text()) })
+    return res.send({ data: $('section[data-test="qsp-statistics"]').get().map(val => $(val).text()) })
 
     try {
 
